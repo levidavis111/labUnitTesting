7690491741f2a46431bf747d50b6601289bde252
levi davis <levidavis@pursuit.org>
2019-08-29 17:04:15 -0400

    Sets up trivia view controller and cells

--- lab-unit-testing/lab-unit-testing/View Controllers/RandomFactViewController.swift
+++ lab-unit-testing/lab-unit-testing/View Controllers/RandomFactViewController.swift
@@ -9,36 +9,50 @@
 import UIKit
 
 class RandomFactViewController: UIViewController {
+    
+    var randomFacts = [Fact]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
 
     @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        loadData()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func loadData() {
+        guard let pathToJSONFile = Bundle.main.path(forResource: "randomfacts", ofType: "json") else {fatalError("Could not find path")}
+        let url = URL(fileURLWithPath: pathToJSONFile)
+        do {
+            let data = try Data(contentsOf: url)
+            let factsFromJSON = ArrayOfFacts.getFacts(from: data)
+            randomFacts = factsFromJSON
+        } catch let loadDataError {
+            fatalError("Error: \(loadDataError)")
+        }
     }
-    */
 
 }
 
 extension RandomFactViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return randomFacts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "randomFactCell", for: indexPath)
+        let oneFact = randomFacts[indexPath.row]
+        cell.textLabel?.text = oneFact.question.removingPercentEncoding
+        return cell
+        
     }
     
     
