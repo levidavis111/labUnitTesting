9e165030dac478115031b33c217e2695e8c87acb
levi davis <levidavis@pursuit.org>
2019-08-29 17:10:04 -0400

    Sets up detail view controllers in storyboard

---
+++ lab-unit-testing/lab-unit-testing/Detail View Controllers/RandomFactDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RandomFactDetailViewController.swift
+//  lab-unit-testing
+//
+//  Created by Levi Davis on 8/29/19.
+//  Copyright © 2019 Levi Davis. All rights reserved.
+//
+
+import UIKit
+
+class RandomFactDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
